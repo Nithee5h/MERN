febea8ebd3f32336dc6f3d0606e2ba50386ba824
Paurush Kumar <115066832+Blink29@users.noreply.github.com>
2025-04-11 13:40:32 +0000

    backend started

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json("hello world");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`);
+})
