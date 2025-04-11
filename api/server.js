367401bef8d53dd9d8b234452785fded910f4670
Paurush Kumar <115066832+Blink29@users.noreply.github.com>
2025-04-11 14:18:08 +0000

    feat: backend done

--- api/server.js
+++ api/server.js
@@ -11,11 +11,20 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.json("hello world");
 });
 
 app.use("/api/todos", todoRoutes)
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({message: "Something went wrong"});
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
